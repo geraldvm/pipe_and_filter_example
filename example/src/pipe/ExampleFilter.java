af2613dd52a9e40142524d0320222a4b26369e3d
Gerald Valverde <gerald-_16@hotmail.com>
2021-10-24 23:04:19 -0600

    Pipe and Filter example

---
+++ example/src/pipe/ExampleFilter.java
@@ -0,0 +1,15 @@
+package pipe;
+
+public class ExampleFilter extends SimpleFilter<Integer, String> {
+    public ExampleFilter(Pipe<Integer> input, Pipe<String> output) {
+        super(input, output);
+    }
+
+    @Override
+    protected String transformOne(Integer in) {
+        String out = Integer.toString(in);
+        System.out.println("filtered " + Integer.toString(in) + " to " + out);
+        delayForDebug(100);
+        return out;
+    }
+}
---
+++ example/src/pipe/SimpleFilter.java
@@ -0,0 +1,4 @@
+package pipe;
+
+public class SimpleFilter {
+}
