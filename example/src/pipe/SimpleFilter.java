8d7bf6dc769343396137ac54a1b960fec875e07e
Gerald Valverde <gerald-_16@hotmail.com>
2021-10-24 23:09:00 -0600

    Update Changes

--- example/src/pipe/SimpleFilter.java
+++ example/src/pipe/SimpleFilter.java
@@ -1,4 +1,25 @@
 package pipe;
+public abstract class SimpleFilter<I, O> extends Filter<I, O> {
+    public SimpleFilter(Pipe<I> input, Pipe<O> output) {
+        super(input, output);
+    }
 
-public class SimpleFilter {
+    @Override
+    protected void transformBetween(Pipe<I> input, Pipe<O> output) {
+        try {
+            I in;
+            while ((in = input.nextOrNullIfEmptied()) != null) {
+                O out = transformOne(in);
+                output.put(out);
+            }
+        } catch (InterruptedException e) {
+            // TODO handle properly, using advice in http://www.ibm.com/developerworks/java/library/j-jtp05236/
+            System.err.println("interrupted");
+            e.printStackTrace();
+            return;
+        }
+        output.closeForWriting();
+    }
+
+    protected abstract O transformOne(I in);
 }
